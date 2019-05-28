1a50a6abe9892f2dbc89c3aecd55b72e718e590b
maks <maksimlya@gmail.com>
2019-05-28 20:20:18 +0300

    Statistics update

--- src/components/Statistics/Graphs/Graphs.js
+++ src/components/Statistics/Graphs/Graphs.js
@@ -6,6 +6,7 @@ import TooltipGraph from './TooltipGraph/TooltipGraph';
 import LineGraph from './LineGraph/LineGraph';
 import CandleChart from './CandleChart/CandleChart'
 import Dropdown from '../../CreatePoll/Dropdown/Dropdown'
+import "./Graphs.module.css"
 import Parse from 'parse'
 
 class Graphs extends Component {
@@ -17,6 +18,8 @@ class Graphs extends Component {
             pollId: 0,
             cityData: new Map(),
             countryData: new Map(),
+            genderData: new Map(),
+            religionData: new Map(),
             isLoading: true,
             candleChartLoaded: false,
             colors: [
@@ -36,13 +39,17 @@ class Graphs extends Component {
         });
 
         await this.setState({ options: optionss, polls: polls});
+        this.setState({candleChartLoaded:true})
 
         if(this.state.polls.length > 0) {
+
             await this.loadCitiesData();
             await this.loadCountriesData();
+            await this.loadGenderData();
+            await this.loadReligionData();
         }
 
-        this.setState({candleChartLoaded:true})
+
 
     }
 
@@ -51,49 +58,42 @@ class Graphs extends Component {
         await this.setState({isLoading: true});
         await this.setState({pollId: e.id});
         await this.setState({candleChartLoaded: true});
-        await this.loadCitiesData();
-        await this.loadCountriesData();
+
+         await this.loadCitiesData();
+         await this.loadCountriesData();
+         await this.loadGenderData();
+         await this.loadReligionData();
         await this.setState({isLoading: false});
+
+
 
 
 
     };
 
 
-    functionWithPromise = item => { //a function that returns a promise
-        return Promise.resolve(item)
-    }
-    anAsyncFunction = async item => {
-        return await this.functionWithPromise(item)
-    }
-    getData = async (list) => {
-        return await Promise.all(list.map(item => this.anAsyncFunction(item)))
-    }
 
+     async loadCitiesData() {
+         let cityData = new Map();
+         let users = Parse.Object.extend('User');
+         let choices =  this.state.polls[this.state.pollId].choices;
 
+         for(let choice of choices){
+             let cities = [];
+             if(this.state.polls[this.state.pollId].results.Voters[choice])
+                 for(let pubKey of this.state.polls[this.state.pollId].results.Voters[choice]){
+                     let query = new Parse.Query(users);
+                     query.equalTo('pubKey', pubKey)
+                     let usr = await query.find();
 
+                     cities.push(usr[0].get('country'))
+                 }
+             cityData.set(choice,cities);
+         }
 
-     async loadCitiesData(){
-        let cityData = new Map();
-        let users = Parse.Object.extend('User');
+             await this.setState({cityData: cityData});
 
-
-            let choices =  this.state.polls[this.state.pollId].choices;
-            for(let choice of choices){
-                let cities = [];
-                if(this.state.polls[this.state.pollId].results.Voters[choice])
-                for(let pubKey of this.state.polls[this.state.pollId].results.Voters[choice]){
-                    let query = new Parse.Query(users);
-                    query.equalTo('pubKey', pubKey)
-                    let usr = await query.find();
-
-                    cities.push(usr[0].get('city'))
-                }
-                cityData.set(choice,cities);
-            }
-
-            await this.setState({cityData: cityData});
-    }
+     }
 
     async loadCountriesData(){
         let countryData = new Map();
@@ -117,26 +117,71 @@ class Graphs extends Component {
         await this.setState({countryData: countryData});
     }
 
+    async loadGenderData(){
+        let genderData = new Map();
+        let users = Parse.Object.extend('User');
+
+
+        let choices =  this.state.polls[this.state.pollId].choices;
+        for(let choice of choices){
+            let genders = [];
+            if(this.state.polls[this.state.pollId].results.Voters[choice])
+                for(let pubKey of this.state.polls[this.state.pollId].results.Voters[choice]){
+                    let query = new Parse.Query(users);
+                    query.equalTo('pubKey', pubKey)
+                    let usr = await query.find();
+
+                    genders.push(usr[0].get('gender'))
+                }
+            genderData.set(choice,genders);
+        }
+
+        await this.setState({genderData: genderData});
+    }
+    async loadReligionData(){
+        let religionData = new Map();
+        let users = Parse.Object.extend('User');
+
+
+        let choices =  this.state.polls[this.state.pollId].choices;
+        for(let choice of choices){
+            let religions = [];
+            if(this.state.polls[this.state.pollId].results.Voters[choice])
+                for(let pubKey of this.state.polls[this.state.pollId].results.Voters[choice]){
+                    let query = new Parse.Query(users);
+                    query.equalTo('pubKey', pubKey)
+                    let usr = await query.find();
+
+                    religions.push(usr[0].get('religion'))
+                }
+            religionData.set(choice,religions);
+        }
+
+        await this.setState({religionData: religionData});
+    }
+
     render() {
         let users = Parse.Object.extend('User');
         var cities = new Map();
         return (
-        <div className={styles.Graphs}>
+        <div className= {styles.Graphs}>
             <Dropdown items={this.state.options} handleChange={this.handleChange}/>
-
             {this.state.candleChartLoaded &&
-            <CandleChart data={this.state.polls[this.state.pollId]}/>
+                <div className={styles.Candle}>
+            <CandleChart  data={this.state.polls[this.state.pollId]}/>
+                </div>
             }
+
             {!this.state.isLoading &&
                 <div>
 
-
-
+<div>
+                    <Row className= {styles.cityLabel}><Form.Label>Cities Distribution</Form.Label></Row>
             <Row>
 
 
                 { this.state.polls[this.state.pollId].choices.map( (val,id) => {
-                    return <Col key={id}><Form.Label>{val}</Form.Label><PieGraph data={this.state.cityData.get(val)}/></Col>
+                    return <Col className={styles.Col} key={id}><Form.Label className={styles.pollLabel}>{val}</Form.Label><PieGraph data={this.state.cityData.get(val)}/></Col>
                 })
 
 
@@ -146,11 +191,13 @@ class Graphs extends Component {
 
 
             </Row>
+</div>
+                    <Row className= {styles.cityLabel}><Form.Label>Countries Distribution</Form.Label></Row>
+
                     <Row>
 
-
                         { this.state.polls[this.state.pollId].choices.map( (val,id) => {
-                            return <Col key={id}><Form.Label>{val}</Form.Label><PieGraph data={this.state.countryData.get(val)}/></Col>
+                            return <Col className={styles.Col} key={id}><Form.Label className={styles.pollLabel}>{val}</Form.Label><PieGraph data={this.state.countryData.get(val)}/></Col>
                         })
 
 
@@ -160,6 +207,38 @@ class Graphs extends Component {
 
 
                     </Row>
+
+                    <Row className= {styles.cityLabel}><Form.Label>Genders Distribution</Form.Label></Row>
+
+                    <Row>
+
+                        { this.state.polls[this.state.pollId].choices.map( (val,id) => {
+                            return <Col className={styles.Col} key={id}><Form.Label className={styles.pollLabel}>{val}</Form.Label><PieGraph data={this.state.genderData.get(val)}/></Col>
+                        })
+
+
+
+                        }
+
+
+
+                    </Row>
+
+                    <Row className= {styles.cityLabel}><Form.Label>Religions Distribution</Form.Label></Row>
+                    <Row>
+
+                        { this.state.polls[this.state.pollId].choices.map( (val,id) => {
+                            return <Col className={styles.Col} key={id}><Form.Label className={styles.pollLabel}>{val}</Form.Label><PieGraph data={this.state.religionData.get(val)}/></Col>
+                        })
+
+
+
+                        }
+
+
+
+                    </Row>
+
                 </div>
             }
 
